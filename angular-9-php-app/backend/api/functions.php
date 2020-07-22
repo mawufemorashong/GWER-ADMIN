18c4546a7766cc3facf9e537cc108c804a8d3ff1
mikey <micjay13@gmail.com>
2020-07-22 16:47:44 +0000

    php backend added

---
+++ angular-9-php-app/backend/api/functions.php
@@ -0,0 +1,50 @@
+<?php 
+
+
+
+    function sanitizeString($var){
+    global $connection;
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    $var = stripslashes($var);
+    return $connection->real_escape_string($var); 
+}
+
+
+
+function queryMysql($query){
+    global $connection;
+    $result = $connection->query($query);
+    if (!$result) die($connection->error);
+    return $result;
+}
+
+
+
+function encodePassword($password){
+    $salt1 = "qm&h*a";
+    $salt2 = "rt0*^p";
+    $token = hash('ripemd128', "$salt1$password$salt2");
+    return $token;
+}
+
+
+function getUserData($user_id){
+    $user_result = queryMysql("SELECT * FROM `clients` WHERE `id` = '$user_id'");
+    if($user_result->num_rows){
+        $result = $user_result->fetch_array(MYSQLI_ASSOC);
+    }
+    else $result = null;
+    return $result;
+}
+
+
+
+
+
+
+
+
+
+
+
